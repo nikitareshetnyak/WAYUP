1c3dd2020041366fc7300730d7ad962569ff9a51
IT genius <71625912+nikitareshetnyak@users.noreply.github.com>
2020-09-23 08:34:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.title-second ').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.ways').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+	
+})
